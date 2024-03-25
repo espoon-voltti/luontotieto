84996aa6f54f402ba8d0c6fd58d32ecdd5fed241
Ilja <ilja-git@users.noreply.github.com>
2024-03-25 14:57:41 +0200

    Option to keep container running

--- etl/migrate.sh
+++ etl/migrate.sh
@@ -4,4 +4,13 @@
 #
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
+set -euo pipefail
+
 ./gradlew --offline --no-daemon flywayMigrate
+
+if [ "${1:-}" = "--wait" ]; then
+    echo "Waiting"
+    while true; do
+        sleep 30
+    done
+fi
