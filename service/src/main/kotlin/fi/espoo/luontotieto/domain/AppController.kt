de124421c3b7f02e6bc93162d3419c6311086c99
Topi Penttinen <5527844+n0ps@users.noreply.github.com>
2024-04-03 10:28:32 +0300

    Initial GeoPackage reader implementation (#32)
    * Add secondary database paikkatieto
    
    * Add initial GeoPackage reader implementation

--- service/src/main/kotlin/fi/espoo/luontotieto/domain/AppController.kt
+++ service/src/main/kotlin/fi/espoo/luontotieto/domain/AppController.kt
@@ -6,23 +6,73 @@ package fi.espoo.luontotieto.domain
 
 import fi.espoo.luontotieto.config.AuthenticatedUser
 import fi.espoo.luontotieto.config.audit
+import fi.espoo.paikkatieto.domain.LiitoOravaAlueet
+import fi.espoo.paikkatieto.domain.LiitoOravaPisteet
+import fi.espoo.paikkatieto.domain.LiitoOravaYhteysviivat
+import fi.espoo.paikkatieto.domain.insertLiitoOravaAlueet
+import fi.espoo.paikkatieto.domain.insertLiitoOravaPisteet
+import fi.espoo.paikkatieto.domain.insertLiitoOravaYhteysviivat
+import fi.espoo.paikkatieto.reader.GpkgReader
 import mu.KotlinLogging
 import org.jdbi.v3.core.Jdbi
 import org.jdbi.v3.core.kotlin.inTransactionUnchecked
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import java.io.File
 import java.util.UUID
 
 @RestController
 class AppController {
+    @Qualifier("jdbi-luontotieto")
     @Autowired
     lateinit var jdbi: Jdbi
 
+    @Qualifier("jdbi-paikkatieto")
+    @Autowired
+    lateinit var paikkatietoJdbi: Jdbi
+
     private val logger = KotlinLogging.logger {}
+
+    @PostMapping("/file")
+    fun uploadFile(): String {
+        val reader1 =
+            GpkgReader(
+                File("src/test/resources/test-data/liito_orava_pisteet.gpkg"),
+                LiitoOravaPisteet
+            )
+        val data1 = reader1.asSequence()
+
+        val reader2 =
+            GpkgReader(
+                File("src/test/resources/test-data/liito_orava_alueet.gpkg"),
+                LiitoOravaAlueet
+            )
+        val data2 = reader2.asSequence()
+
+        val reader3 =
+            GpkgReader(
+                File("src/test/resources/test-data/liito_orava_yhteysviivat.gpkg"),
+                LiitoOravaYhteysviivat
+            )
+        val data3 = reader3.asSequence()
+
+        paikkatietoJdbi.inTransactionUnchecked { tx ->
+            tx.insertLiitoOravaPisteet(data1)
+            tx.insertLiitoOravaAlueet(data2)
+            tx.insertLiitoOravaYhteysviivat(data3)
+        }
+
+        reader1.close()
+        reader2.close()
+        reader3.close()
+
+        return "OK"
+    }
 
     data class StudentAndCaseInput(
         val student: StudentInput,
@@ -33,15 +83,12 @@ class AppController {
         user: AuthenticatedUser,
         @RequestBody body: StudentAndCaseInput
     ): UUID {
-        return jdbi.inTransactionUnchecked { tx ->
-            val studentId = tx.insertStudent(data = body.student, user = user)
-            studentId
-        }.also {
-            logger.audit(
-                user,
-                "CREATE_STUDENT"
-            )
-        }
+        return jdbi
+            .inTransactionUnchecked { tx ->
+                val studentId = tx.insertStudent(data = body.student, user = user)
+                studentId
+            }
+            .also { logger.audit(user, "CREATE_STUDENT") }
     }
 
     data class StudentResponse(
@@ -53,15 +100,11 @@ class AppController {
         user: AuthenticatedUser,
         @PathVariable id: UUID
     ): StudentResponse {
-        return jdbi.inTransactionUnchecked { tx ->
-            val studentDetails = tx.getStudent(id = id)
-            StudentResponse(studentDetails)
-        }.also {
-            logger.audit(
-                user,
-                "GET_STUDENT",
-                mapOf("studentId" to id.toString())
-            )
-        }
+        return jdbi
+            .inTransactionUnchecked { tx ->
+                val studentDetails = tx.getStudent(id = id)
+                StudentResponse(studentDetails)
+            }
+            .also { logger.audit(user, "GET_STUDENT", mapOf("studentId" to id.toString())) }
     }
 }
---
+++ service/src/main/kotlin/fi/espoo/paikkatieto/reader/GpkgReader.kt
@@ -0,0 +1,52 @@
+// SPDX-FileCopyrightText: 2023-2024 City of Espoo
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+package fi.espoo.paikkatieto.reader
+
+import fi.espoo.paikkatieto.domain.TableDefinition
+import mu.KotlinLogging
+import org.geotools.api.data.SimpleFeatureReader
+import org.geotools.geopkg.GeoPackage
+import java.io.Closeable
+import java.io.File
+import java.io.IOException
+
+private val logger = KotlinLogging.logger {}
+
+class GpkgReaderException(msg: String) : IOException(msg)
+
+class GpkgReader(file: File, private val tableDefinition: TableDefinition) :
+    Iterator<Map<String, Any?>>, Closeable {
+    private val gpkg: GeoPackage = GeoPackage(file)
+    private val reader: SimpleFeatureReader
+
+    init {
+        val featureEntry =
+            gpkg.features().firstOrNull()
+                ?: throw GpkgReaderException("Unable to find feature table")
+        reader = gpkg.reader(featureEntry, null, null)
+    }
+
+    override fun hasNext(): Boolean {
+        return reader.hasNext()
+    }
+
+    override fun next(): Map<String, Any> {
+        val feature = reader.next()
+        return tableDefinition.columns.associateWith { feature.getAttribute(it) }
+    }
+
+    override fun close() {
+        try {
+            reader.close()
+        } catch (e: IOException) {
+            logger.error("Unable to close SimpleFeatureReader", e)
+        }
+        try {
+            gpkg.close()
+        } catch (e: IOException) {
+            logger.error("Unable to close GeoPackage", e)
+        }
+    }
+}
