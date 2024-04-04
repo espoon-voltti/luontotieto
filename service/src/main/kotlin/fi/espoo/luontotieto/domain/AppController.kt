7d1a31976bd78f92a8c42cdde17fb0472b2eee3a
RichardPeltonen <richard.peltonen@reaktor.com>
2024-04-04 14:10:39 +0300

    Fix imports

--- service/src/main/kotlin/fi/espoo/luontotieto/domain/AppController.kt
+++ service/src/main/kotlin/fi/espoo/luontotieto/domain/AppController.kt
@@ -23,7 +23,13 @@ import org.jdbi.v3.core.kotlin.inTransactionUnchecked
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.http.MediaType
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RequestPart
+import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
 import java.io.File
 import java.util.UUID
