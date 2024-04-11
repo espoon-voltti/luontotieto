40bc8601af9b4da881eb45b307f8c1b9e157df2f
Topi Penttinen <5527844+n0ps@users.noreply.github.com>
2024-04-11 12:46:32 +0300

    Fix order tests

--- service/src/test/kotlin/fi/espoo/luontotieto/OrderTests.kt
+++ service/src/test/kotlin/fi/espoo/luontotieto/OrderTests.kt
@@ -8,7 +8,6 @@ import fi.espoo.luontotieto.domain.AppController
 import fi.espoo.luontotieto.domain.DocumentType
 import fi.espoo.luontotieto.domain.OrderInput
 import fi.espoo.luontotieto.domain.OrderReportDocument
-import fi.espoo.luontotieto.domain.OrderReportDocumentInput
 import org.springframework.beans.factory.annotation.Autowired
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -21,8 +20,8 @@ class OrderTests : FullApplicationTest() {
     fun `create order with all data and fetch`() {
         val orderReportDocuments =
             listOf(
-                OrderReportDocumentInput("Test description", DocumentType.LIITO_ORAVA_PISTEET),
-                OrderReportDocumentInput("Test description 2", DocumentType.LIITO_ORAVA_ALUEET)
+                OrderReportDocument("Test description", DocumentType.LIITO_ORAVA_PISTEET),
+                OrderReportDocument("Test description 2", DocumentType.LIITO_ORAVA_ALUEET)
             )
         val createdOrderId =
             controller.createOrderFromScratch(
@@ -40,8 +39,8 @@ class OrderTests : FullApplicationTest() {
         assertEquals(
             orderResponse.reportDocuments,
             listOf(
-                OrderReportDocument(orderId = createdOrderId, "Test description", DocumentType.LIITO_ORAVA_PISTEET),
-                OrderReportDocument(orderId = createdOrderId, "Test description 2", DocumentType.LIITO_ORAVA_ALUEET)
+                OrderReportDocument("Test description", DocumentType.LIITO_ORAVA_PISTEET),
+                OrderReportDocument("Test description 2", DocumentType.LIITO_ORAVA_ALUEET)
             )
         )
     }
