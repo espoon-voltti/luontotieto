c175f1dfe29f8cebd33a4e392746e48e1ebc4602
Topi Penttinen <5527844+n0ps@users.noreply.github.com>
2024-04-17 10:42:30 +0300

    Create Back-navigation component

---
+++ frontend/src/shared/buttons/BackNavigation.tsx
@@ -0,0 +1,41 @@
+// SPDX-FileCopyrightText: 2023-2024 City of Espoo
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons'
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { FlexColWithGaps, SectionContainer, VerticalGap } from 'shared/layout'
+import { BaseProps } from 'shared/theme'
+import { H3 } from 'shared/typography'
+
+import { InlineButton } from './InlineButton'
+
+interface Props extends BaseProps {
+  text: string
+  destination?: string
+}
+
+export const BackNavigation = React.memo(function BackNavigation({
+  'data-qa': dataQa,
+  ...props
+}: Props) {
+  const navigate = useNavigate()
+
+  return (
+    <>
+      <SectionContainer>
+        <FlexColWithGaps $gapSize="s">
+          <InlineButton
+            data-qa={dataQa}
+            icon={faChevronLeft}
+            text="Takaisin"
+            onClick={() => navigate(props.destination ?? '/luontotieto')}
+          />
+          <H3>{props.text}</H3>
+        </FlexColWithGaps>
+      </SectionContainer>
+      <VerticalGap $size="m" />
+    </>
+  )
+})
