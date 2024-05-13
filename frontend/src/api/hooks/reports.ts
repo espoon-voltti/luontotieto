38b32ebdfcaa823337fddf9f35b5008fc4202605
Topi Penttinen <5527844+n0ps@users.noreply.github.com>
2024-05-13 09:57:49 +0300

    Add missing license

--- frontend/src/api/hooks/reports.ts
+++ frontend/src/api/hooks/reports.ts
@@ -1,37 +1,41 @@
-import { useQuery } from '@tanstack/react-query'
-import { apiGetReport, apiGetReportFiles, apiGetReports } from 'api/report-api'
+// SPDX-FileCopyrightText: 2023-2024 City of Espoo
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+import {useQuery} from '@tanstack/react-query'
+import {apiGetReport, apiGetReportFiles, apiGetReports} from 'api/report-api'
 
 export function useGetReportsQuery() {
-  return useQuery({
-    queryKey: ['reports'],
-    queryFn: () => apiGetReports()
-  })
+    return useQuery({
+        queryKey: ['reports'],
+        queryFn: () => apiGetReports()
+    })
 }
 
 export function useGetReportQuery(id?: string) {
-  return useQuery({
-    queryKey: ['report', id],
-    queryFn: () => {
-      //TODO: a bit hacky find out if theres better way to pass conditional params
-      // the enabled param should however prevent that this will not fire without the id
-      if (id) {
-        return apiGetReport(id)
-      }
-      return null
-    },
-    enabled: !!id
-  })
+    return useQuery({
+        queryKey: ['report', id],
+        queryFn: () => {
+            //TODO: a bit hacky find out if theres better way to pass conditional params
+            // the enabled param should however prevent that this will not fire without the id
+            if (id) {
+                return apiGetReport(id)
+            }
+            return null
+        },
+        enabled: !!id
+    })
 }
 
 export function useGetReportFilesQuery(id?: string) {
-  return useQuery({
-    queryKey: ['reportFiles', id],
-    queryFn: () => {
-      if (id) {
-        return apiGetReportFiles(id)
-      }
-      return null
-    },
-    enabled: !!id
-  })
+    return useQuery({
+        queryKey: ['reportFiles', id],
+        queryFn: () => {
+            if (id) {
+                return apiGetReportFiles(id)
+            }
+            return null
+        },
+        enabled: !!id
+    })
 }
