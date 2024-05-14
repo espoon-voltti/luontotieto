0ef6b2667b95bc47a5b45611e21f5c0aecbbd912
RichardPeltonen <richard.peltonen@reaktor.com>
2024-05-14 14:47:20 +0300

    lint

--- service/src/main/kotlin/fi/espoo/luontotieto/SystemController.kt
+++ service/src/main/kotlin/fi/espoo/luontotieto/SystemController.kt
@@ -13,7 +13,6 @@ import fi.espoo.luontotieto.common.upsertAppUserFromAd
 import fi.espoo.luontotieto.config.AuditEvent
 import fi.espoo.luontotieto.config.AuthenticatedUser
 import fi.espoo.luontotieto.config.audit
-import java.util.UUID
 import mu.KotlinLogging
 import org.jdbi.v3.core.Jdbi
 import org.jdbi.v3.core.kotlin.inTransactionUnchecked
@@ -26,6 +25,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
 
 data class PasswordUser(val email: String, val password: String)
 
@@ -36,40 +36,49 @@ data class PasswordUser(val email: String, val password: String)
 @RestController
 @RequestMapping("/system")
 class SystemController {
-    @Qualifier("jdbi-luontotieto") @Autowired lateinit var jdbi: Jdbi
+    @Qualifier("jdbi-luontotieto")
+    @Autowired
+    lateinit var jdbi: Jdbi
 
     private val logger = KotlinLogging.logger {}
 
     @PostMapping("/user-login")
-    fun adLogin(user: AuthenticatedUser, @RequestBody adUser: AdUser): AppUser {
+    fun adLogin(
+        user: AuthenticatedUser,
+        @RequestBody adUser: AdUser
+    ): AppUser {
         return jdbi.inTransactionUnchecked { it.upsertAppUserFromAd(adUser, user) }.also {
             logger.audit(user, AuditEvent.USER_LOGIN)
         }
     }
 
     @PostMapping("/password-login")
-    fun passwordLogin(@RequestBody passwordUser: PasswordUser): AppUser {
+    fun passwordLogin(
+        @RequestBody passwordUser: PasswordUser
+    ): AppUser {
         return jdbi
-                .inTransactionUnchecked {
-                    val user = it.getAppUserWithPassword(passwordUser.email)
-                    if (user == null) {
-                        logger.info("Login attempt failed. Invalid email.")
-                        throw Unauthorized()
-                    }
-
-                    val encoder = Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8()
-
-                    if (!encoder.matches(passwordUser.password, user.password)) {
-                        logger.info("Login attempt failed. Invalid password.")
-                        throw Unauthorized()
-                    }
-                    user.toAppUser()
+            .inTransactionUnchecked {
+                val user = it.getAppUserWithPassword(passwordUser.email)
+                if (user == null) {
+                    logger.info("Login attempt failed. Invalid email.")
+                    throw Unauthorized()
                 }
-                .also { logger.audit(AuthenticatedUser(it.id), AuditEvent.PASSWORD_LOGIN) }
+
+                val encoder = Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8()
+
+                if (!encoder.matches(passwordUser.password, user.password)) {
+                    logger.info("Login attempt failed. Invalid password.")
+                    throw Unauthorized()
+                }
+                user.toAppUser()
+            }
+            .also { logger.audit(AuthenticatedUser(it.id), AuditEvent.PASSWORD_LOGIN) }
     }
 
     @GetMapping("/users/{id}")
-    fun getUser(@PathVariable id: UUID): AppUser? {
+    fun getUser(
+        @PathVariable id: UUID
+    ): AppUser? {
         return jdbi.inTransactionUnchecked { it.getAppUser(id) }
     }
 }
