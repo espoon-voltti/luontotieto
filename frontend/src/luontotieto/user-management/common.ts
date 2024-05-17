539b942cb0251e35ad6ddbdd59764647cc295b79
RichardPeltonen <richard.peltonen@reaktor.com>
2024-05-17 12:24:30 +0300

    Fix regex

--- frontend/src/luontotieto/user-management/common.ts
+++ frontend/src/luontotieto/user-management/common.ts
@@ -2,4 +2,4 @@
 //
 // SPDX-License-Identifier: LGPL-2.1-or-later
 
-export const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,{2,63}}$/g
+export const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,64}$/g
