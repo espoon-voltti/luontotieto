fb02359ecba64eae0fa7a7610fec8c71ffd73e33
RichardPeltonen <richard.peltonen@reaktor.com>
2024-05-22 12:07:17 +0300

    Add additional rules

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -4,24 +4,41 @@
 
 import eslint from '@eslint/js'
 import typescriptEslint from 'typescript-eslint'
+import reactHooks from 'eslint-plugin-react-hooks'
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 export default [
-  { ignores: ['build.js','dist/*', '.yarn/*'] },
+  { ignores: ['build.js', 'dist/*', '.yarn/*'] },
   ...typescriptEslint.config(
     eslint.configs.recommended,
     ...typescriptEslint.configs.recommended,
     ...typescriptEslint.configs.stylistic
   ),
   {
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.eslint.json'
+      }
+    },
+    plugins: { 'react-hooks': reactHooks },
     rules: {
+      '@typescript-eslint/no-misused-promises': [
+        'error',
+        {
+          checksVoidReturn: false
+        }
+      ],
       '@typescript-eslint/no-unused-vars': [
         'warn',
         {
           argsIgnorePattern: '^_',
           varsIgnorePattern: '^_'
         }
-      ]
+      ],
+      '@typescript-eslint/consistent-type-definitions': 'off',
+      '@typescript-eslint/prefer-nullish-coalescing': 'off',
+      '@typescript-eslint/prefer-optional-chain': 'off',
+      '@typescript-eslint/no-var-requires': 'off'
     }
   },
   eslintPluginPrettierRecommended
