6e4c2730ec475d6528d63109a58e3cb46846def9
Topi Penttinen <5527844+n0ps@users.noreply.github.com>
2024-05-28 12:19:59 +0300

    Create config Bean LuontotietoHost

---
+++ service/src/main/kotlin/fi/espoo/luontotieto/config/LuontotietoHost.kt
@@ -0,0 +1,23 @@
+// SPDX-FileCopyrightText: 2023-2024 City of Espoo
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+package fi.espoo.luontotieto.config
+
+import org.apache.http.client.utils.URIBuilder
+import org.springframework.core.env.Environment
+import java.util.UUID
+
+data class LuontotietoHost(val host: String) {
+    fun getReportUrl(id: UUID): String {
+        val builder = URIBuilder(host)
+        builder.path = "/luontotieto/selvitys/$id"
+        return builder.build().toString()
+    }
+
+    companion object {
+        fun fromEnvironment(env: Environment): LuontotietoHost {
+            return LuontotietoHost(env.lookup("luontotieto.host"))
+        }
+    }
+}
