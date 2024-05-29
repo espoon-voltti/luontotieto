bdc5dbbdf5be2b7e20c738039c9cbfae8d44e0e9
Topi Penttinen <5527844+n0ps@users.noreply.github.com>
2024-05-29 08:10:21 +0300

    Use ordering unit and make selvitys_id not nullable

--- service/src/main/resources/db/paikkatieto/migration/V006__create_aluerajaus_tables.sql
+++ service/src/main/resources/db/paikkatieto/migration/V006__create_aluerajaus_tables.sql
@@ -4,7 +4,7 @@ CREATE TABLE aluerajaus_luontoselvitystilaus
     tilauksen_nimi   TEXT                    NOT NULL,
     tilauksen_tekija TEXT                    NOT NULL,
     tilausyksikko    TEXT                    NOT NULL,
-    selvitys_id      UUID                    NULL,
+    selvitys_id      UUID                    NOT NULL,
     selvitys_linkki  TEXT                    NOT NULL,
     geom             geometry(POLYGON, 3879) NOT NULL
 );
@@ -19,7 +19,7 @@ CREATE TABLE aluerajaus_luontoselvitys
     selvitys_tekija TEXT                    NOT NULL,
     tilausyksikko   TEXT                    NOT NULL,
     pinta_ala       REAL                    NOT NULL GENERATED ALWAYS AS (st_area(geom) / 10000) STORED,
-    selvitys_id     UUID                    NULL,
+    selvitys_id     UUID                    NOT NULL,
     selvitys_linkki TEXT                    NOT NULL,
     geom            geometry(POLYGON, 3879) NOT NULL
 );
