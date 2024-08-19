ee70726eb14e8f8d369cfe78db1581ff8c63ea8e
RichardPeltonen <richard.peltonen@reaktor.com>
2024-08-19 06:29:20 +0300

    Add colum comments

--- service/src/main/resources/db/paikkatieto/migration/V018_add_free_schema_tables.sql
+++ service/src/main/resources/db/paikkatieto/migration/V018_add_free_schema_tables.sql
@@ -15,7 +15,7 @@ CREATE INDEX lajiryhma_alueet_idx ON lajiryhma_alueet USING gist (geom);
 
 COMMENT ON COLUMN lajiryhma_alueet.pvm IS 'Päivämäärä, jolloin havainto on maastossa tehty';
 COMMENT ON COLUMN lajiryhma_alueet.havaitsija IS 'Jos havainto on tehty toimeksiannon pohjalta, tulee yrityksen virallinen nimi (Oy:t mukaan). Jos yksityishenkilö, käytetään "-"-viivamerkkiä yksityisyyden suojan vuoksi. Havaitsijataho voi olla myös Espoon kaupunki.';
-COMMENT ON COLUMN lajiryhma_alueet.lajiryhma IS 'TODO:';
+COMMENT ON COLUMN lajiryhma_alueet.lajiryhma IS 'Lajiryhmä, jolle alue on merkittävä. Esim. linnut.';
 COMMENT ON COLUMN lajiryhma_alueet.aluekuvaus IS 'Alueen merkitys lajiryhmälle, esim. paikallisesti arvokas lintualue';
 COMMENT ON COLUMN lajiryhma_alueet.lisatieto IS 'Vapaaehtoinen kenttä, joka jätetään suurimmaksi osaksi tyhjäksi. Tähän voi kirjoittaa, jos kohteessa tai ympäristössä on jotain omasta mielestä mainitsemisen arvoista.';
 COMMENT ON COLUMN lajiryhma_alueet.viite IS 'Päivämäärä, jolloin havainto on maastossa tehty';
@@ -40,8 +40,8 @@ CREATE INDEX arvokkaat_luontokohteet_alueet_idx ON arvokkaat_luontokohteet_aluee
 
 COMMENT ON COLUMN arvokkaat_luontokohteet_alueet.pvm IS 'Päivämäärä, jolloin havainto on maastossa tehty';
 COMMENT ON COLUMN arvokkaat_luontokohteet_alueet.havaitsija IS 'Jos havainto on tehty toimeksiannon pohjalta, tulee yrityksen virallinen nimi (Oy:t mukaan). Jos yksityishenkilö, käytetään "-"-viivamerkkiä yksityisyyden suojan vuoksi. Havaitsijataho voi olla myös Espoon kaupunki.';
-COMMENT ON COLUMN arvokkaat_luontokohteet_alueet.nimi IS 'TODO:';
-COMMENT ON COLUMN arvokkaat_luontokohteet_alueet.arvoluokka IS 'TODO:';
+COMMENT ON COLUMN arvokkaat_luontokohteet_alueet.nimi IS 'Kohteen nimi. Kenttään täytetään kohdetta kuvaava nimi, kuten uomanosuuden nimi tai kohteen tunnettu kutsumanimi. Esim. Nettaan purolaakso tai Sahajärven lehtokorpi.';
+COMMENT ON COLUMN arvokkaat_luontokohteet_alueet.arvoluokka IS 'Kohteen arvon sanallinen kuvaus. Esim. paikallisesti arvokas, maakunnallisesti arvokas, muu säilyttämisen arvoinen luontokohde';
 COMMENT ON COLUMN arvokkaat_luontokohteet_alueet.aluekuvaus IS 'Alueen merkitys lajiryhmälle, esim. paikallisesti arvokas lintualue';
 COMMENT ON COLUMN arvokkaat_luontokohteet_alueet.lisatieto IS 'Vapaaehtoinen kenttä, joka jätetään suurimmaksi osaksi tyhjäksi. Tähän voi kirjoittaa, jos kohteessa tai ympäristössä on jotain omasta mielestä mainitsemisen arvoista.';
 COMMENT ON COLUMN arvokkaat_luontokohteet_alueet.viite IS 'Päivämäärä, jolloin havainto on maastossa tehty';
@@ -65,13 +65,12 @@ CREATE TABLE arvokkaat_vesikohteet_alueet
 
 CREATE INDEX arvokkaat_vesikohteet_alueet_idx ON arvokkaat_vesikohteet_alueet USING gist (geom);
 
--- TODO: Add comments for the basic fields
 COMMENT ON COLUMN arvokkaat_vesikohteet_alueet.pvm IS 'Päivämäärä, jolloin havainto on maastossa tehty';
 COMMENT ON COLUMN arvokkaat_vesikohteet_alueet.havaitsija IS 'Jos havainto on tehty toimeksiannon pohjalta, tulee yrityksen virallinen nimi (Oy:t mukaan). Jos yksityishenkilö, käytetään "-"-viivamerkkiä yksityisyyden suojan vuoksi. Havaitsijataho voi olla myös Espoon kaupunki.';
-COMMENT ON COLUMN arvokkaat_vesikohteet_alueet.nimi IS 'TODO:';
-COMMENT ON COLUMN arvokkaat_vesikohteet_alueet.arvoluokka IS 'TODO:';
+COMMENT ON COLUMN arvokkaat_vesikohteet_alueet.nimi IS 'Kohteen nimi. Kenttään täytetään esimerkiksi uomanosuuden nimi, vesialueen nimi tai kohteen tunnettu kutsumanimi.';
+COMMENT ON COLUMN arvokkaat_vesikohteet_alueet.arvoluokka IS 'Kohteen arvon sanallinen kuvaus. Esim. paikallisesti arvokas, maakunnallisesti arvokas, luonnontilainen puro';
 COMMENT ON COLUMN arvokkaat_vesikohteet_alueet.aluekuvaus IS 'Alueen merkitys lajiryhmälle, esim. paikallisesti arvokas lintualue';
-COMMENT ON COLUMN arvokkaat_vesikohteet_alueet.lisatieto IS 'TODO:';
+COMMENT ON COLUMN arvokkaat_vesikohteet_alueet.vesisto IS 'TODO:';
 COMMENT ON COLUMN arvokkaat_vesikohteet_alueet.lisatieto IS 'Vapaaehtoinen kenttä, joka jätetään suurimmaksi osaksi tyhjäksi. Tähän voi kirjoittaa, jos kohteessa tai ympäristössä on jotain omasta mielestä mainitsemisen arvoista.';
 COMMENT ON COLUMN arvokkaat_vesikohteet_alueet.viite IS 'Päivämäärä, jolloin havainto on maastossa tehty';
 COMMENT ON COLUMN arvokkaat_vesikohteet_alueet.pinta_ala IS 'Alueen koko hehtaareina';
