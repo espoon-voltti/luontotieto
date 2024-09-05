63e198f2df10429b05a89ba7936c1576ac78ce49
RichardPeltonen <richard.peltonen@reaktor.com>
2024-09-05 06:00:17 +0300

    Fix file title bug

--- frontend/src/shared/form/File/FileTitle.tsx
+++ frontend/src/shared/form/File/FileTitle.tsx
@@ -34,7 +34,7 @@ export const Link = styled(StyledLink)`
 export const FileTitle = React.memo(function FileTitle(props: Props) {
   return (
     <Label>
-      {`${getDocumentTypeTitle(props.documentType)} ${props.required && '*'}`}
+      {`${getDocumentTypeTitle(props.documentType)} ${props.required ? '*' : ''}`}
       {isReportFileDocumentType(props.documentType) &&
         props.documentType !== ReportFileDocumentType.OTHER &&
         props.documentType !== ReportFileDocumentType.REPORT && (
