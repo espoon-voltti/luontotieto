e2b77c5bb469f36f1e1b923d588f6f3d0eb2e422
RichardPeltonen <richard.peltonen@reaktor.com>
2024-12-17 10:59:41 +0200

    Fix CVE errors

--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -53,7 +53,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-    implementation("org.springframework.ws:spring-ws-security")
+    implementation("org.springframework.ws:spring-ws-security") {
+        exclude("org.opensaml")
+    }
 
     implementation("org.geotools:gt-geopkg:32.1")
     implementation("org.geotools:gt-epsg-hsql:32.1")
