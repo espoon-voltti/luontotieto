941d841fc2dd232c4e7aa75544c6e9e0831449b0
Väinö Jalonen <vaino.jalonen@reaktor.com>
2025-03-13 12:13:38 +0200

    Change migration to populate the year column from return_date for existing orders

--- service/src/main/resources/db/luontotieto/migration/V0026__add_year_column_to_order.sql
+++ service/src/main/resources/db/luontotieto/migration/V0026__add_year_column_to_order.sql
@@ -1,3 +1,3 @@
-ALTER table "order" ADD COLUMN year integer NOT NULL DEFAULT EXTRACT(year FROM CURRENT_DATE)::integer;
-
-UPDATE "order" SET year = EXTRACT(YEAR FROM created)::integer;
+ALTER table "order" ADD COLUMN year integer;
+UPDATE "order" SET year = EXTRACT(YEAR FROM return_date)::integer;
+ALTER table "order" ALTER COLUMN year SET NOT NULL;
