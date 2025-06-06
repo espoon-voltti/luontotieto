762f0d47e12ae5463c7f7fd0e545aab66a30cd62
Joosa Kurvinen <joosa.kurvinen@reaktor.com>
2025-06-06 14:53:20 +0300

    ok

--- frontend/src/auth/UserMenu.tsx
+++ frontend/src/auth/UserMenu.tsx
@@ -111,7 +111,7 @@ export const UserMenu = React.memo(function LanguageMenu({
     >
       <DropDownButton
         onClick={toggleOpen}
-        data-qa="logged-in-user-foobar"
+        data-qa="logged-in-user"
         aria-expanded={open}
         aria-haspopup="true"
       >
