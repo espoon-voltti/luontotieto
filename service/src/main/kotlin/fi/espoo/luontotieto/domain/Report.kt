61e72b44a539b4a72494e0e1ee30f8a6be5bd571
Joonas Reynders <joonas.reynders@reaktor.fi>
2026-02-03 15:07:30 +0200

    Fix isPublic serialization broken by jackson upgdate

--- service/src/main/kotlin/fi/espoo/luontotieto/domain/Report.kt
+++ service/src/main/kotlin/fi/espoo/luontotieto/domain/Report.kt
@@ -4,6 +4,7 @@
 
 package fi.espoo.luontotieto.domain
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import fi.espoo.luontotieto.common.NotFound
 import fi.espoo.luontotieto.common.databaseValue
 import fi.espoo.luontotieto.common.sanitizeCsvCellData
@@ -25,6 +26,7 @@ data class Report(
     val updated: OffsetDateTime,
     val createdBy: String,
     val updatedBy: String,
+    @get:JsonProperty("isPublic")
     val isPublic: Boolean?,
     val noObservations: List<DocumentType>?,
     val observedSpecies: List<String>?,
@@ -34,6 +36,7 @@ data class Report(
     companion object {
         data class ReportInput(
             val name: String,
+            @get:JsonProperty("isPublic")
             val isPublic: Boolean?,
             val noObservations: List<DocumentType>?
         )
